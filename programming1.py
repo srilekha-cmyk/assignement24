637154c4e18a0acf61d87d09cc1b8a60b23adb51
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 13:51:29 +0530

    Implement square asterisk pattern generator
    Add a program to print a square of asterisks based on user input.

---
+++ programming1.py
@@ -0,0 +1,13 @@
+n = int(input("Enter size: "))
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+# Output:
+# Enter size: 4
+# * * * *
+# * * * *
+# * * * *
+# * * * *
