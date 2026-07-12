5fac016f8a547958f6617e92383e7ae19493834d
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 13:55:59 +0530

    Add pyramid pattern generator in programming3.py
    This script generates a pyramid pattern based on user-defined height.

---
+++ programming3.py
@@ -0,0 +1,16 @@
+n = int(input("Enter height: "))
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
+
+# Output:
+# Enter height: 5
+#     *
+#    *
+#   ***
+#  ***
+# ***
